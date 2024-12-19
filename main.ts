dfd3f425ec8fc9c13b18dfef428c71dd0b0fefd0
Amanda Benjamin <159163860+Amanda-Benjamin@users.noreply.github.com>
2024-12-19 09:25:56 -0500

    Added In Music For Backing Up & Turning + New Faces

--- main.ts
+++ main.ts
@@ -1,23 +1,21 @@
-let DIAMETER = 0
-let TRACK_LENGTH = 0
-let HIGH_ANGLE = 0
-let LOW_ANGLE = 0
-let sonarPing = 0
+let sonoarping = 0
 basic.forever(function () {
-    basic.showIcon(IconNames.Ghost)
-    DIAMETER = 31
-    TRACK_LENGTH = 20
-    HIGH_ANGLE = 270
-    LOW_ANGLE = 45
-    robotbit.StpCarMove(1, 20)
-    sonarPing = sonar.ping(
+    basic.showIcon(IconNames.Happy)
+    robotbit.StpCarMove(2, 31)
+    sonoarping = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )
-    if (sonarPing <= 5) {
-        basic.showIcon(IconNames.Skull)
-        robotbit.StpCarMove(-10, DIAMETER)
-        robotbit.StpCarTurn(randint(LOW_ANGLE, HIGH_ANGLE), DIAMETER, TRACK_LENGTH)
+    if (sonoarping <= 8) {
+        basic.showIcon(IconNames.Sad)
+        for (let index = 0; index < 5; index++) {
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+            robotbit.StpCarMove(-2, 31)
+        }
+        basic.showIcon(IconNames.Silly)
+        music.play(music.stringPlayable("B A G B A G B C5 ", 500), music.PlaybackMode.UntilDone)
+        robotbit.StpCarTurn(randint(45, 270), 31, 20)
+        music.play(music.stringPlayable("B A G B A G C5 C ", 500), music.PlaybackMode.UntilDone)
     }
 })

f342047ccd8e62c5b935fb0e0728f57555fbd3ec
Amanda Benjamin <159163860+Amanda-Benjamin@users.noreply.github.com>
2025-01-07 09:14:55 -0500

    added new code

--- main.ts
+++ main.ts
@@ -5,31 +5,6 @@ let LOW_ANGLE = 0
 let WALL_DISTANCE = 0
 let sonoarping = 0
 basic.forever(function () {
-    DIAMETER = 31
-    TRACK_LENGTH = 20
-    HIGH_ANGLE = 270
-    LOW_ANGLE = 45
-    WALL_DISTANCE = 8
-    basic.showIcon(IconNames.Happy)
-    robotbit.StpCarMove(2, DIAMETER)
-    sonoarping = sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    )
-    if (sonoarping <= WALL_DISTANCE) {
-        basic.showIcon(IconNames.Sad)
-        for (let index = 0; index < 5; index++) {
-            music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
-            robotbit.StpCarMove(-2, DIAMETER)
-        }
-        basic.showIcon(IconNames.Silly)
-        music.play(music.stringPlayable("B A G B A G B C5 ", 500), music.PlaybackMode.UntilDone)
-        robotbit.StpCarTurn(randint(LOW_ANGLE, HIGH_ANGLE), DIAMETER, TRACK_LENGTH)
-        music.play(music.stringPlayable("B A G B A G C5 C ", 500), music.PlaybackMode.UntilDone)
-    }
-})
-basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
         music.play(music.stringPlayable("E D C C - - - - ", 120), music.PlaybackMode.UntilDone)
         while (true) {
@@ -55,5 +30,31 @@ basic.forever(function () {
                 . . . . .
                 `)
         }
+    }
+})
+basic.forever(function () {
+    DIAMETER = 31
+    TRACK_LENGTH = 200
+    HIGH_ANGLE = 270
+    LOW_ANGLE = 90
+    WALL_DISTANCE = 8
+    basic.showIcon(IconNames.Happy)
+    robotbit.StpCarMove(2, DIAMETER)
+    sonoarping = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    if (sonoarping <= WALL_DISTANCE) {
+        basic.showNumber(sonoarping)
+        basic.showIcon(IconNames.Sad)
+        for (let index = 0; index < 5; index++) {
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+            robotbit.StpCarMove(-2, DIAMETER)
+        }
+        basic.showIcon(IconNames.Silly)
+        music.play(music.stringPlayable("B A G B A G B C5 ", 500), music.PlaybackMode.UntilDone)
+        robotbit.StpCarTurn(randint(LOW_ANGLE, HIGH_ANGLE), DIAMETER, TRACK_LENGTH)
+        music.play(music.stringPlayable("B A G B A G C5 C ", 500), music.PlaybackMode.UntilDone)
     }
 })
